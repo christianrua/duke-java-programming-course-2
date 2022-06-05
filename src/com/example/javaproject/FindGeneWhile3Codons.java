2da8f73f10b6419846b18c48f86f1b699ddc5cd1
christian.rua@gmail.com <christian.rua@gmail.com>
2022-06-05 16:11:34 -0500

    looking multiple codons in dna string

---
+++ src/com/example/javaproject/FindGeneWhile3Codons.java
@@ -0,0 +1,43 @@
+package com.example.javaproject;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class FindGeneWhile3Codons {
+
+    int[] starIndexCodonsArray = new int[3];
+
+    private int findSmallestCodonIndex(String dna, int startIndex) {
+
+        starIndexCodonsArray[0] = dna.indexOf("TAA", startIndex);
+        starIndexCodonsArray[1] = dna.indexOf("TGA", startIndex);
+        starIndexCodonsArray[2] = dna.indexOf("TAG", startIndex);
+
+        int minValue = Arrays.stream(starIndexCodonsArray).min().orElse(-1);
+
+        return minValue;
+
+    }
+
+    public String findGene3Codons(String dna) {
+        int startIndex = dna.indexOf("ATG");
+        int currIndex = findSmallestCodonIndex(dna, startIndex + 3);
+
+        while (currIndex != -1) {
+            if ((currIndex - startIndex) % 3 == 0) {
+                return dna.substring(startIndex, currIndex + 3);
+            } else {
+                currIndex = findSmallestCodonIndex(dna, currIndex + 1);
+            }
+        }
+        return "not gene where find it";
+    }
+
+    public void testFindGeneSimple() {
+        String dna = "ATGATCGCTGATTAGGCTTAAATGACG";
+        System.out.println("DNA strand is " + dna);
+        String gene = findGene3Codons(dna);
+        System.out.println("Gene is " + gene);
+
+    }
+}
--- src/com/example/javaproject/helloWorld.java
+++ src/com/example/javaproject/helloWorld.java
@@ -1,25 +1,11 @@
 package com.example.javaproject;
 
-import com.example.javaproject.FindGeneSimpleAndTest;
-import com.example.javaproject.Part3;
-import com.example.javaproject.Part4;
+import com.example.javaproject.AllCodons;
 
 public class helloWorld {
     public static void main(String[] args) {
-    /*    String s = "dukeprogramming";
-        String x = s.substring(4,7);
-        System.out.println(s);
-        System.out.println(x);
-        System.out.println("length value " + s.length());
-        System.out.println("index of program is " + s.indexOf("program"));*/
 
-        FindGeneSimpleAndTest testingClass = new FindGeneSimpleAndTest();
-        testingClass.testFindGeneSimple();
-
-        /*Part3 testingPart3 = new Part3();
-        testingPart3.testing();*/
-
-        /*Part4 testingPart4 = new Part4();
-        testingPart4.printYouTubeLink();*/
+        AllCodons someTestClass = new AllCodons();
+        someTestClass.testFindStop();
     }
 }
